4203d4d49c4a1dcb6870f6ad3a7ce24d12edca8f
bambam-boy <vittokushankuhibosheri@gmail.com>
2026-09-02 10:49:23 +0330

    added more configs

---
+++ app/core/config.py
@@ -0,0 +1,57 @@
+from pydantic_settings import BaseSettings,SettingsConfigDict
+from typing import Optional
+from pydantic import ValidationInfo,field_validator,PostgresDsn
+
+class Settings(BaseSettings):
+    POSTGRES_USERNAME:str
+    POSTGRES_PASSWORD:str
+    POSTGRES_HOST:str
+    POSTGRES_PORT:str
+    POSTGRES_DB:str
+
+    PORJECT_NAME:str="BINGO SHOP"
+    PORJECT_VERSION:str="0.1.0"
+    API_URL:str="/api/v1"
+
+    SQLALCHEMY_DATABASE_URI:Optional[PostgresDsn]=None
+
+    @field_validator("SQLALCHEMY_DATABASE_URI",mode="before")
+    @classmethod
+    def attach_db_url(cls,v:Optional[str]|None,values:ValidationInfo):
+        if not v is None and not v=="":
+            return v
+        datas=values.data
+        url=PostgresDsn.build(
+            scheme="postgresql",
+            username=datas.get("POSTGRES_USERNAME"),
+            password=datas.get("POSTGRES_PASSWORD"),
+            host=datas.get("POSTGRES_HOST"),
+            port=int(datas.get("POSTGRES_PORT")),
+            path=datas.get("POSTGRES_DB")
+        )
+
+        return url
+
+    SQLALCHEMY_DATABASE_URI_ASYNC:Optional[PostgresDsn]=None
+
+    @field_validator("SQLALCHEMY_DATABASE_URI_ASYNC",mode="before")
+    @classmethod
+    def attach_db_url_async(cls,v:Optional[str]|None,values:ValidationInfo):
+        if not v is None and not v=="":
+            return v
+        datas=values.data
+        url=PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            username=datas.get("POSTGRES_USERNAME"),
+            password=datas.get("POSTGRES_PASSWORD"),
+            host=datas.get("POSTGRES_HOST"),
+            port=int(datas.get("POSTGRES_PORT")),
+            path=datas.get("POSTGRES_DB")
+        )
+
+        return url    
+
+
+    model_config=SettingsConfigDict(env_file=".env")
+
+settings=Settings()
--- app/main.py
+++ app/main.py
@@ -1,3 +1,8 @@
 from fastapi import FastAPI
+from app.core.config import settings
 
-app=FastAPI()
+app=FastAPI(
+    title=settings.PORJECT_NAME,
+    version=settings.PORJECT_VERSION,
+    openapi_url=f"{settings.API_URL}/openapi.json"
+)
